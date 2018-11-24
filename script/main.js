ebc203cdb73f9e0447dcd1ec919edf2f74af0035
Fenriez <andy.dolot@gmail.com>
2018-11-24 14:07:07 +0200

    Function createNewUser recieved new functionality

--- script/main.js
+++ script/main.js
@@ -1,24 +1,29 @@
-let createNewUser = function () {
+let firstName = prompt('Enter your name', '');
+let lastName = prompt('Enter your last name', '');
+
+let createNewUser = function (firstName, lastName) {
     let user = new Object();
 
     user.getFirstName = function() {
-        return prompt('Enter your name', '');
+        this.firstName = firstName; 
     }
     user.getLastName = function() {
-        return prompt('Enter your last name', '');
+        this.lastName = lastName; 
     }
 
     user.getLogin = function() {
-        return this.firstName.charAt(0).toLowerCase() + this.lastName.toLowerCase();
+        user.login = this.firstName.charAt(0).toLowerCase() + this.lastName.toLowerCase();
     }
 
-    user.firstName = user.getFirstName();
-    user.lastName = user.getLastName();
-    user.login = user.getLogin();
+    user.getFirstName();
+    user.getLastName();
+    user.getLogin();
 
     return user;
 }
 
-let newUser = createNewUser();
+let newUser = createNewUser(firstName, lastName);
+let newUser2 = createNewUser('John', 'Smith');
 
-console.log(newUser.login);
+console.log('Your login: ' + newUser.login);
+console.log('Test user: ' + newUser2.login);
